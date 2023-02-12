d662794bcced56d5f6c36d3970cd85a1e53a1b87
Themaytrix <obengraymond81@gmail.com>
2023-02-12 06:03:33 +0000

    ..

---
+++ airbnb/src/components/Hero.js
@@ -0,0 +1,12 @@
+import hero from "../images/hero.png"
+
+export default function Hero(){
+    return(
+        <section className="hero">
+            <img className="hero--photo" src={hero} alt="hero" />
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+</p>
+        </section>
+    )
+};
